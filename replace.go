21a75d59a0ac4841dc81efccdcdbc3ffd44b824e
eyedeekay <hankhill19580@gmail.com>
2024-09-20 20:37:03 -0400

    include os and arch in

--- replace.go
+++ replace.go
@@ -1,16 +1,14 @@
 package shellservice
 
-import "strings"
-
 var OSES = []string{"mac", "linux", "windows"}
 var ARCHES = []string{"386", "amd64", "arm", "arm64"}
 
 func Replace(r string) string {
-	for _, os := range OSES {
+	/*for _, os := range OSES {
 		r = strings.Replace(r, os, "$OS", -1)
 	}
 	for _, arch := range ARCHES {
 		r = strings.Replace(r, arch, "$ARCH", -1)
-	}
+	}*/
 	return r
 }
