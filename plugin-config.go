22d2d54fbd26f126eb2a83d3f416ed677aef6cb1
eyedeekay <hankhill19580@gmail.com>
2024-10-02 22:33:50 -0400

    Make it compatible with the keys I can export from my Java signing keys

--- plugin-config.go
+++ plugin-config.go
@@ -290,12 +290,12 @@ func (pc *PluginConfig) LoadPrivateKey(path string) (*rsa.PrivateKey, error) {
 	}
 
 	privDer, _ := pem.Decode(privPem)
-	privKey, err := x509.ParsePKCS1PrivateKey(privDer.Bytes)
+	privKey, err := x509.ParsePKCS8PrivateKey(privDer.Bytes)
 	if err != nil {
 		return nil, err
 	}
 
-	return privKey, nil
+	return privKey.(*rsa.PrivateKey), nil
 }
 
 func (pc *PluginConfig) keysPath(path string) (string, error) {
